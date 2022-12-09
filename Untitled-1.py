3803b76139be601204e393ae9409230f47e5622e
LOVITOR <118411564+LOVITOR@users.noreply.github.com>
2022-12-09 14:00:00 -0600

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+#definición de variables
+
+
+nombre = input("ingrese su nombre: ")
+apellidos = input("ingrese sus apellidos: ")
+edad = input ("ingrese su edad: ")
+altura = input ("ingrese su altura: ")
+peso = input ("ingrese su peso: ")
+
+IMC = peso / altura**2
+
+print ("Su IMC es: ")
